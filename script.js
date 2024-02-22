ee9d420f2b588b611f99bf1465b2aa8fef1054c2
hssn08 <knocker160@gmail.com>
2024-02-22 21:27:59 +0500

    portfolio

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('Portfolio loaded.');
 
-    // Function to toggle section visibility
     function toggleSection(sectionId) {
         var section = document.getElementById(sectionId);
-        section.classList.toggle("open");
-        
-        // Close other open sections
-        var sections = document.querySelectorAll(".section");
-        sections.forEach(function(sec) {
-            if (sec.id !== sectionId && sec.classList.contains("open")) {
-                sec.classList.remove("open");
-            }
-        });
+        if (section) {
+            section.classList.toggle("open");
+
+            var sections = document.querySelectorAll(".section");
+            sections.forEach(function(sec) {
+                if (sec.id !== sectionId && sec.classList.contains("open")) {
+                    sec.classList.remove("open");
+                }
+            });
+        }
     }
 
-    // Add click event listeners to navigation links
-    var navLinks = document.querySelectorAll("nav ul li a");
-    navLinks.forEach(function(link) {
-        link.addEventListener("click", function(event) {
-            event.preventDefault(); // Prevent default link behavior
-            var targetId = link.getAttribute("href").substring(1); // Get target section ID
-            toggleSection(targetId); // Toggle section visibility
-        });
-    });
-
-    // Smooth scrolling for anchor links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
@@ -35,10 +24,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // Function to show current year in the footer
     document.getElementById('currentYear').innerText = new Date().getFullYear();
 
-    // Animate elements on scroll
     window.addEventListener('scroll', () => {
         const scrollPosition = window.scrollY;
         const sections = document.querySelectorAll('.section');
@@ -51,7 +38,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // Trigger animations on page load
     const elements = document.querySelectorAll('.container, .section h2, .section p');
     elements.forEach(element => {
         element.style.opacity = 0;
